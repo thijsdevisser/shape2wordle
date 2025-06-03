50c6ab25933dc7af0841ec2aa64af976ef9db75a
thijsdevisser <thijsdevisser@live.nl>
2025-06-04 00:22:47 +0200

    sg

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import numpy as np
 from utils import shape2wordle, plot, shape_dict
 
-answer = 'maria'
+answer = 'rough'
 shape = 'alien'
 shape_dict = shape_dict()
-colour = 2
+colour = 0
 
 shape = shape2wordle(answer, shape, colour, shape_dict)
 plot(shape)
