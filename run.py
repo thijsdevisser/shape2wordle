57ea40d85b68d1d903573584efcc39f5421b599b
thijsdevisser <thijsdevisser@live.nl>
2025-06-18 22:02:10 +0200

    sdrf

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import numpy as np
 from utils import shape2wordle, plot, shape_dict
 
-answer = 'cease'
-shape = 'heart'
+answer = 'plaid'
+shape = 'uhh'
 shape_dict = shape_dict()
-colour = 2
+colour = 0
+
 
 shape = shape2wordle(answer, shape, colour, shape_dict)
 plot(shape)
